da14b5e3a6eb952f8cda867d1899b66ae677556d
LukazDs <lukazds2016@gmail.com>
2022-09-22 17:44:20 -0300

    feat: test error in case of conflict -> 'unit'

--- tests/unit/insertRecommendation.test.ts
+++ tests/unit/insertRecommendation.test.ts
@@ -1,4 +1,5 @@
 import * as recommendationFactory from "../factories/recommendationFactory";
+import * as errorUtils from "../../src/utils/errorUtils";
 import { recommendationService } from "../../src/services/recommendationsService";
 import { recommendationRepository } from "../../src/repositories/recommendationRepository";
 
@@ -6,17 +7,35 @@ import { recommendationRepository } from "../../src/repositories/recommendationR
 
 describe("Test insert recommendation", () => {
   it("test insert recommendation in case of success", async () => {
+    const expected = undefined;
+
     jest
       .spyOn(recommendationRepository, "create")
-      .mockImplementationOnce(async () => {});
+      .mockImplementationOnce(async () => {
+        expected;
+      });
 
     const recommendation = await recommendationFactory.createRecommendation();
 
     const result = await recommendationService.insert(recommendation);
 
-    //primeiro passo insere
-    //segndo compara com o crpo que retornou
-
     expect(result).toBeUndefined();
+  });
+
+  it("test error in case of conflict", async () => {
+    const recommendation = await recommendationFactory.createRecommendation();
+    const expectedRepository = { id: 1, ...recommendation, score: 12 };
+    const expectedError = "Recommendations names must be unique";
+
+    jest
+      .spyOn(recommendationRepository, "findByName")
+      .mockResolvedValueOnce(expectedRepository);
+
+    const t = () => {
+      throw new TypeError("Recommendations names must be unique");
+    };
+
+    expect(t).toThrow(TypeError);
+    expect(t).toThrow(expectedError);
   });
 });
